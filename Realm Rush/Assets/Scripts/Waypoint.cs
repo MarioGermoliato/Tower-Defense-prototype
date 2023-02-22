273aecb7865e7f70961b0255ba0498f119705745
MarioGermoliato <mgermoliato@yahoo.com.br>
2023-02-22 14:02:48 -0300

    Finding the path

--- Realm Rush/Assets/Scripts/Waypoint.cs
+++ Realm Rush/Assets/Scripts/Waypoint.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject ballista;
+    [SerializeField] bool isPlaceable;    
+    public bool IsPlaceable { get { return isPlaceable; } }
+    private void OnMouseDown()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        if(isPlaceable)
+        {
+            Instantiate(ballista, transform.position, Quaternion.identity);
+            isPlaceable = false;
+        }
+           
+    }       
+       
 }
